fc543714a216ed3bd02a6015c3e5ce942ebb6c07
Tomasz Nazar <tomeknazz@gmail.com>
2023-12-17 14:34:08 +0100

    Data from file is now read to struct and displayed correctly

--- Candle_Charts.cpp
+++ Candle_Charts.cpp
@@ -13,59 +13,91 @@ struct stock_data {
 	double volume;
 };
 
-int main() {
-	// Open the CSV file
-	ifstream file("intc_us_data.csv");
-	if (!file.is_open()) {
-		cerr << "Error opening the file.\n";
-		return 1;
+ifstream open_file(const string& file_name)
+{
+	// Open the file
+	ifstream file(file_name);
+	if (!file.is_open())
+	{
+		cout << "Error opening the file.\n";
+		exit(1);
 	}
+	return file;
+}
 
-	// Determine the number of lines in the file
-	int num_lines = 0;
-	string line;
-	while (getline(file, line)) {
-		++num_lines;
-	}
-
-	// Close and reopen the file to reset the file position
-	file.clear();
-	file.seekg(0, ios::beg);
-
-	// Dynamically allocate the array
-	stock_data* stock_data_list = new stock_data[num_lines];
+long int read_line_numbers(ifstream& file)
+{
+	long int line_count = 0;
 
 	// Read the file line by line
-	int count = 0;
-	while (count < num_lines && getline(file, line)) {
-		istringstream iss(line);
+	std::string line;
+	while (getline(file, line)) {
+		++line_count;
+	}
+
+	// Reset the file position
+	file.clear();
+	file.seekg(0, std::ios::beg);
+
+	return line_count;
+}
+
+void read_csv_to_struct(stock_data*& data, const string& file_name)
+{
+	// Open the CSV file
+	ifstream file = open_file(file_name);
+	const long int line_count = read_line_numbers(file);
+	data = new stock_data[line_count];
+	// Skip the header line
+	string header;
+	getline(file, header);
+
+	// Read the file line by line
+	for (int count = 0; count < line_count; ++count) {
+		std::string line;
+		getline(file, line);
+		std::istringstream iss(line);
 
 		// Read each column from the line
-		if (iss.getline(stock_data_list[count].date, sizeof(stock_data_list[count].date), ',') &&
-			getline(iss, line, ',') && (istringstream(line) >> stock_data_list[count].open) &&
-			getline(iss, line, ',') && (istringstream(line) >> stock_data_list[count].high) &&
-			getline(iss, line, ',') && (istringstream(line) >> stock_data_list[count].low) &&
-			getline(iss, line, ',') && (istringstream(line) >> stock_data_list[count].close) &&
-			getline(iss, line, ',') && (istringstream(line) >> stock_data_list[count].volume)) {
-			++count;
+		if (iss.getline(data[count].date, sizeof(data[count].date), ',') &&
+			getline(iss, line, ',') && (std::istringstream(line) >> data[count].open) &&
+			getline(iss, line, ',') && (std::istringstream(line) >> data[count].high) &&
+			getline(iss, line, ',') && (std::istringstream(line) >> data[count].low) &&
+			getline(iss, line, ',') && (std::istringstream(line) >> data[count].close) &&
+			getline(iss, line, ',') && (std::istringstream(line) >> data[count].volume)) {
+			// Data successfully read
 		}
 		else {
-			cerr << "Error parsing line: " << line << endl;
+			std::cerr << "Error parsing line: " << line << std::endl;
 		}
 	}
 
 	// Close the file
 	file.close();
+}
 
-	// Display the read data
-	for (int i = 0; i < num_lines; ++i) {
-		cout << "Date: " << stock_data_list[i].date << ", Open: " << stock_data_list[i].open
-			<< ", High: " << stock_data_list[i].high << ", Low: " << stock_data_list[i].low
-			<< ", Close: " << stock_data_list[i].close << ", Volume: " << stock_data_list[i].volume << endl;
+void print_stock_data(stock_data* data, long int line_count)
+{
+
+	for (int i = 0; i < line_count - 1; ++i)
+	{
+		cout << "Date: " << data[i].date << ", Open: " << data[i].open
+			<< ", High: " << data[i].high << ", Low: " << data[i].low
+			<< ", Close: " << data[i].close << endl;
 	}
+}
 
-	// Don't forget to free the dynamically allocated memory
+int main() {
+	string file_name;
+	cout << "Enter the file name: ";
+	cin >> file_name;
+	ifstream file = open_file(file_name);
+	// Create an array to store StockData
+	stock_data* stock_data_list = nullptr;
+	read_csv_to_struct(stock_data_list, file_name);
+	print_stock_data(stock_data_list, read_line_numbers(file));
+
+
 	delete[] stock_data_list;
-
 	return 0;
 }
